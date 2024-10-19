479dc93b479785d2f12811649b9c5a04d6942984
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-10-19 17:48:03 +0530

    Add `class instance variables` & `class methods`
     + Added class instance variable -> "@available_positions" along with getter & setter method as "attr_accessor".
     + Any instance of this class can able to access & modify this variable.
     + Additionally, "valid_position?(number)" class method is added to operate on the class level.
    
    Due to the requirement of shared state, a "class instance variable" and "class methods" are used.
    It's use case comes handy in "Edge case scenario -> Choosing the already selected position".
    If this method is unavailable, the place where 'X' is present can be replaces with 'O' by choosing same position.
    This method helps in filtering out the positions by removing the used positions from the class instance array.

--- tic_tac_toe/lib/player.rb
+++ tic_tac_toe/lib/player.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 # This class deals with player objects like player's name, player's flag and player's choice.
-# These methods serves as a template to create player objects.
+# These methods serves as a template to create player objects. It also contains class instance variables and methods.
 class Player
+  @available_positions = (1..9).to_a
+
+  class << self
+    attr_accessor :available_positions
+  end
+
+  def self.valid_position?(position)
+    return false unless available_positions.include?(position)
+
+    available_positions.delete(position)
+    true
+  end
+
   def name(number)
     puts '                                                                                                           '
     puts '+ ------------------------------------------------------------------------------------------------------- +'
@@ -25,6 +38,13 @@ class Player
     selection
   end
 
+  def valid_position?(number)
+    return false unless valid_positions.include?(number)
+
+    valid_positions.delete(number)
+    true
+  end
+
   def position_of_choice(player, player_banner)
     position = ''
     puts '                                                                                                           '
@@ -33,7 +53,7 @@ class Player
       print "  #{player}, Enter the available placeholder number (1-9) to put the '#{player_banner}' in it's place: "
       position = gets.chomp.to_i
       puts '+ ------------------------------------------------------------------------------------------------------- +'
-      break if position >= 1 && position <= 9
+      break if self.class.valid_position?(position)
     end
     position
   end
