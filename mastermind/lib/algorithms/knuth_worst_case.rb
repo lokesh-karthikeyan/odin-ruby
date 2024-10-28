a719547b58b1e4e26f1328b9d61dca9e42a2578b
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-10-28 16:34:20 +0530

    Add `guess()` method to make a guess
     + Required the "Minimax" class from a different file and instantiated inside this class.
     + Added methods to filter the possibilities list & choose the best guess from the heuristic data.
     + Modified the "attr_reader" access methods to public.
    
    The access modifier change is so that other classes can check if the guess is a valid one.

--- mastermind/lib/algorithms/knuth_worst_case.rb
+++ mastermind/lib/algorithms/knuth_worst_case.rb
@@ -1,18 +1,32 @@
 # frozen_string_literal: true
 
 require 'set'
+require_relative 'minimax'
 
 # Donald Knuth's algorithm to guess the correct code within five guesses.
 class WorstCase
   private
 
-  attr_reader :possibilities, :probability_of_a_possibility
-
   def initialize
-    possibilities = Set.new
-    %w[1 2 3 4 5 6].repeated_permutation(4) { |permutation| possibilities << permutation.join }
-    generate_dictionary_of_probabilities
+    @all_possibilities = Set.new
+    %w[1 2 3 4 5 6].repeated_permutation(4) { |permutation| all_possibilities << permutation.join }
+    @current_possibilities = all_possibilities.dup
   end
 
-  def generate_dictionary_of_probabilities; end
+  def filter_possibilities(guess, feedback_of_the_guess)
+    current_possibilities.reject! { |possibility| possibility == guess }
+
+    current_possibilities.keep_if do |possibility|
+      feedback_of_the_guess == outcome_of_the_guess(possibility, guess)
+    end
+  end
+
+  public
+
+  def guess
+    potential_answers = Minimax.new(current_possibilities, all_possibilities)
+    potential_answers.best_guess
+  end
+
+  attr_reader :all_possibilities, :current_possibilities
 end
