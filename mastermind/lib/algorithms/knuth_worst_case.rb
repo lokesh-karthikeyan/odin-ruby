bc12256357372da741770bab38d5d7df02d58d04
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-10-28 20:03:53 +0530

    Rearrange `methods()` inside the code base
     + Removed the "private" access modifier and so the methods are now set to default which is public.
     + Required the necessary files such as different classes & modules.
    
    Private methods are removed as it was unable to handle the methods inside the module.
    As there are only three methods including "initialize", hence removed the private access modifiers.

--- mastermind/lib/algorithms/knuth_worst_case.rb
+++ mastermind/lib/algorithms/knuth_worst_case.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 require 'set'
+require_relative '../feedback'
 require_relative 'minimax'
 
 # Donald Knuth's algorithm to guess the correct code within five guesses.
+# Find the list of worst case guesses & choose the least worst case guess.
 class WorstCase
-  private
+  attr_reader :all_possibilities, :current_possibilities
 
   def initialize
     @all_possibilities = Set.new
@@ -13,20 +15,19 @@ class WorstCase
     @current_possibilities = all_possibilities.dup
   end
 
-  def filter_possibilities(guess, feedback_of_the_guess)
-    current_possibilities.reject! { |possibility| possibility == guess }
+  include Feedback
 
-    current_possibilities.keep_if do |possibility|
-      feedback_of_the_guess == outcome_of_the_guess(possibility, guess)
+  # Remove the current_guessed_code if it's not the right answer & keep only the codes that has same feedback.
+  def filter_possibilities(guessed_code, feedback_of_the_guess)
+    current_possibilities.reject! { |possibility| possibility == guessed_code }
+
+    current_possibilities.keep_if do |possibile_guess|
+      feedback_of_the_guess == outcome_of_the_guess(possibile_guess, guessed_code)
     end
   end
-
-  public
 
   def guess
     potential_answers = Minimax.new(current_possibilities, all_possibilities)
     potential_answers.best_guess
   end
-
-  attr_reader :all_possibilities, :current_possibilities
 end
