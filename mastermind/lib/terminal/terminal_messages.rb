81c1bb6cca34e58cefcc4a134ef9def14b65e4b4
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-10-29 22:49:43 +0530

    Add methods to show `feedback` & `results`
     + "update_feedback()" method is called after each guess to show the feedback of the entered guess.
     + "player_is_won()" & "player_is_lost()" methods is called after the end of the each round.
    
    To show the results in nice format these methods are used.
    The correct color is marked with a "tick", misplaced with transparent "circle" and the wrong one with "X" unicodes.
    Each color is shown with a dot which later will be colorized.

--- mastermind/lib/terminal/terminal_messages.rb
+++ mastermind/lib/terminal/terminal_messages.rb
@@ -17,5 +17,49 @@ module TerminalMessages
     make_player_to_guess
   end
 
+  def update_feedback(score)
+    score_in_symbols = convert_score_to_symbols(score)
+    puts '+----------•----------+'
+    print '|'
+    score_in_symbols.each { |score_symbol| print "  #{score_symbol}  " }
+    puts ' |'
+    puts '+----------•----------+'
+  end
+
+  def convert_score_to_symbols(score_pegs)
+    score_pegs = score_pegs.chars
+    score_pegs.map! { |peg| symbols_of_score[peg] }
+    score_pegs
+  end
+
+  def symbols_of_score
+    {
+      'B' => "\u{2714}",
+      'W' => "\u{25EF}",
+      'X' => "\u{1F5D9}"
+    }
+  end
+
+  def player_is_won
+    puts "Awesome! You've won in #{guess} guesses"
+    show_logs
+  end
+
+  def player_is_lost
+    puts "You've lost! All #{guess} guesses have been used."
+    show_logs
+    puts 'The right combination is:- '
+    show_answer
+  end
+
+  def show_answer
+    answer = convert_code_to_colors(secret_color_code)
+    puts '+--------------•--------------+'
+    print '|     '
+    answer.each { |_color| print '  ⬤  ' }
+    puts '     |'
+    puts '+--------------•--------------+'
+  end
+
   def make_computer_to_guess; end
 end
