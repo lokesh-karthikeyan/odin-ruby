967129f9b20ebf878f783bf78f963c418fc6c3ae
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-10-30 14:03:36 +0530

    Add `Colors` to the terminal messages
     + By using the "using" keyword to include "ColoredText" module, colored the terminal messages.
     + Feedback & guesses are also colored.

--- mastermind/lib/terminal/terminal_messages.rb
+++ mastermind/lib/terminal/terminal_messages.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 require_relative '../feedback'
+require_relative 'colored_text'
 
 # This Module contains methods related to terminal interface.
 # Such as the messages to be displayed while asking a guess & making a guess.
 module TerminalMessages
   include Feedback
+  using ColoredText
 
   def make_player_to_guess
     print "Enter your guess (#{guess}/#{max_guess}): "
@@ -13,17 +15,17 @@ module TerminalMessages
   end
 
   def notify_invalid_guess
-    puts 'Invalid guess!!! Make a valid guess as per the format with the available colors.'
+    puts 'Invalid guess!!! Make a valid guess as per the format with the available colors.'.color_it('red')
     make_player_to_guess
   end
 
   def update_feedback(score)
     score_in_symbols = convert_score_to_symbols(score)
-    puts '+----------•----------+'
-    print '|'
+    puts '+----------•----------+'.color_it('cyan')
+    print '|'.color_it('cyan')
     score_in_symbols.each { |score_symbol| print "  #{score_symbol}  " }
-    puts ' |'
-    puts '+----------•----------+'
+    puts ' |'.color_it('cyan')
+    puts '+----------•----------+'.color_it('cyan')
   end
 
   def convert_score_to_symbols(score_pegs)
@@ -34,31 +36,31 @@ module TerminalMessages
 
   def symbols_of_score
     {
-      'B' => "\u{2714}",
-      'W' => "\u{25EF}",
-      'X' => "\u{1F5D9}"
+      'B' => "\u{2714}".color_it('green'),
+      'W' => "\u{25EF}".color_it('yellow'),
+      'X' => "\u{1F5D9}".color_it('red')
     }
   end
 
   def player_is_won
-    puts "Awesome! You've won in #{guess} guesses"
+    puts "Awesome! You've won in #{guess} guesses".color_it('green')
     show_logs
   end
 
   def player_is_lost
-    puts "You've lost! All #{guess} guesses have been used."
+    puts "You've lost! All #{guess} guesses have been used.".color_it('red')
     show_logs
-    puts 'The right combination is:- '
+    puts 'The right combination is:- '.color_it('bright_green')
     show_answer
   end
 
   def show_answer
     answer = convert_code_to_colors(secret_color_code)
-    puts '+--------------•--------------+'
-    print '|     '
-    answer.each { |_color| print '  ⬤  ' }
-    puts '     |'
-    puts '+--------------•--------------+'
+    puts '+--------------•--------------+'.color_it('bright_green')
+    print '|     '.color_it('bright_green')
+    answer.each { |color| print "  #{color}  " }
+    puts '     |'.color_it('bright_green')
+    puts '+--------------•--------------+'.color_it('bright_green')
   end
 
   def make_computer_to_guess; end
