68107098535c09e28d1aea77b8631249fa1b8149
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-10-30 12:22:04 +0530

    Add `module: ColoredText` to color strings
     + Module contains a set of colors with a corresponding rgb value.
     + This class uses "refine" & "using" method to extend the "String" class.
    
    Instead of regular base class extension, these methods extends only when needed by using "using" keyword.

---
+++ mastermind/lib/terminal/colored_text.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Module handles Monkey-patching for the class 'String'.
+# Any text can be colored by calling the 'refine' method by 'using' this module.
+module ColoredText
+  COLORS = {
+    'green' => '119;221;119',
+    'red' => '255;71;101',
+    'blue' => '119;170;221',
+    'purple' => '133;101;196',
+    'orange' => '255;133;71',
+    'yellow' => '255;225;71',
+    'cyan' => '0;255;255',
+    'steel_blue' => '70;130;180',
+    'bright_green' => '102;255;0'
+  }.freeze
+
+  refine String do
+    def color_it(color)
+      color_in_rgb = COLORS[color]
+      "\e[38;2;#{color_in_rgb}m#{self}\e[0m"
+    end
+  end
+end
