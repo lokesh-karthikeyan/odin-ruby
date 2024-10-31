9555caa5d86e50912a809a552666b1cdd2d3bb6f
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-10-31 17:23:38 +0530

    Add `show_rules()` method to convey rules
     + Created methods with brief details on how the player needs to guess.

--- mastermind/lib/terminal/terminal_messages.rb
+++ mastermind/lib/terminal/terminal_messages.rb
@@ -16,8 +16,32 @@ module TerminalMessages
   def introduction
     print_mastermind_title
     puts "You're a #{'CODE BREAKER'.color_it('steel_blue')} and I'm the #{'CODE MAKER'.color_it('steel_blue')}.".color_it('light_cyan') # rubocop:disable Layout/LineLength
-    puts "Can you guess the right color combinations within #{'12'.color_it('red')} moves?".color_it('light_cyan')
+    puts "Can you guess the right color combination within #{'12'.color_it('red')} moves?".color_it('light_cyan')
     puts ''
+    show_rules
+  end
+
+  def show_rules
+    print 'NOTE: '.color_it('red')
+    puts "Each color in a guess should be separated by a #{'COMMA(,)'.color_it('steel_blue')}.".color_it('light_cyan')
+    puts ''
+    show_guess_format
+    puts ''
+    show_available_colors
+    puts ''
+  end
+
+  # rubocop:disable Layout/LineLength
+  def show_guess_format
+    puts "For exmaple:- #{'Red, Green, Blue, Purple'.color_it('red')} or #{'YELLOW,PURPLE,BLUE,BLUE'.color_it('red')}.".color_it('light_cyan')
+    puts "You can either use #{'UPCASE'.color_it('steel_blue')} or #{'DOWNCASE'.color_it('steel_blue')}.".color_it('light_cyan')
+  end
+  # rubocop:enable Layout/LineLength
+
+  def show_available_colors
+    print 'The available colors are'.color_it('light_cyan')
+    colors.each_value { |colored_color| print " #{colored_color}" }
+    puts '.'.color_it('light_cyan')
   end
 
   def make_player_to_guess
@@ -32,22 +56,26 @@ module TerminalMessages
 
   def update_feedback(score)
     score_in_symbols = score_to_colored_symbols(score)
+    puts ''
     puts '+----------•----------+'.color_it('cyan')
     print '|'.color_it('cyan')
     score_in_symbols.each { |score_symbol| print "  #{score_symbol}  " }
     puts ' |'.color_it('cyan')
     puts '+----------•----------+'.color_it('cyan')
+    puts ''
   end
 
   def player_is_won
     puts ''
-    puts "Awesome! You've won in #{guess - 1} guesses".color_it('green')
+    puts "Awesome! You've won in #{guess - 1} guesses.".color_it('green')
+    puts ''
     show_logs
   end
 
   def player_is_lost
     puts ''
     puts "You've lost! All #{guess - 1} guesses have been used.".color_it('red')
+    puts ''
     show_logs
     puts ''
     puts 'The right combination is:- '.color_it('bright_green')
