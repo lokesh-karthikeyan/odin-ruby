26f2ecc79331151f9d6af57afcd7d5f37c518f5d
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-03 12:12:18 +0530

    Add `module: ColorCodes` hash
     + This module contains a hash dictionary where each color is associated with their codes.

---
+++ mastermind/lib/color_codes.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# This Module is like a dictionary that contains the codes of the available colors.
+module ColorCodes
+  def color_of_a_code
+    {
+      'green' => '1',
+      'red' => '2',
+      'blue' => '3',
+      'purple' => '4',
+      'orange' => '5',
+      'yellow' => '6'
+    }
+  end
+end
