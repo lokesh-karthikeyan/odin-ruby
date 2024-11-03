2cdcb6bacd0fc7b4c8fc3bf7750ca40951aa1eba
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-03 12:25:38 +0530

    Add `class: PrintGuess` to print guesses
     + This class takes in a "guess" as a parameter.
     + "to_color_name()" converts guess in numbers to color names, "to_colored_icons()" replaces name with icons & apply colors.
     + "public" method "my_guess()" prints out the guess inside the terminal.
    
    Now, each guess can be printed inside the terminal with better UI instead of plain numbers.

---
+++ mastermind/lib/print_guess.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative 'colored_text'
+require_relative 'color_codes'
+require_relative 'color_icons'
+
+# This Class gets the guess(numeric) & converts to color names.
+# It then applies colors to the color icons with appropriate color.
+class PrintGuess
+  private
+
+  include ColorCodes
+  include ColorIcons
+  using ColoredText
+
+  def initialize(guessed_code)
+    @guessed_code = guessed_code.chars
+    to_color_name
+    to_colored_icons
+  end
+
+  def to_color_name
+    @guessed_code.map! { |code| color_of_a_code.key(code) }
+  end
+
+  def to_colored_icons
+    @guessed_code.map! { |color_name| apply_colors[color_name] }
+  end
+
+  public
+
+  attr_reader :guessed_code
+
+  def my_guess(guess, max_guess)
+    puts "My guess (#{guess}/#{max_guess}) is:- ".color_it('light_cyan')
+    puts ''
+    puts '+--------------•--------------+'.color_it('cyan')
+    print '|  '.color_it('cyan')
+    @guessed_code.each { |colored_icon| print "   #{colored_icon}  " }
+    puts '   |'.color_it('cyan')
+    puts '+--------------•--------------+'.color_it('cyan')
+  end
+end
