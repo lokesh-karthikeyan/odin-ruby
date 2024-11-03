7d3fd9538274236aca0e6261ded4f2b7d75e81bd
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-03 17:30:52 +0530

    Add `class: PrintWinner` to print the winner
     + It has only class methods to print the winner, either a player (or) a computer.

---
+++ mastermind/lib/print_winner.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative 'colored_text'
+
+# This Class has methods to announce the winner of the current round.
+class PrintWinner
+  class << self
+    using ColoredText
+
+    def player(guess)
+      puts ''
+      puts "Awesome! You've won in #{guess - 1} guesses.".color_it('green')
+      puts ''
+    end
+
+    def computer(guess)
+      puts ''
+      puts "Easy Peasy! I've won in just #{guess - 1} guesses.".color_it('green')
+      puts ''
+    end
+  end
+end
