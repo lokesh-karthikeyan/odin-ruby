6d6e49cc64e0f89cbee106fe5646623b50fd64b0
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-04 18:02:04 +0530

    Add `class: GameOver` to indicate game is ended
     + It calls methods from other classes, depending whether the game is won (or) lost.
     + If it's a player it calls player related methods, else computer related methods.
     + It also calls other classes such as "PrintLogs" & "PrintAnswer".
    
    Most of the ending stuff has been grouped into a folder & this method acts like a core of it.
    Instead of calling two (or) three different classes, we can instantiate this class & calls method from this class.

---
+++ mastermind/lib/game_over/game_over.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require_relative 'print_winner'
+require_relative 'print_loser'
+require_relative 'print_logs'
+require_relative 'print_answer'
+require_relative 'colored_text'
+
+# This Class acts like a central class where it creates instances of different classes.
+# Depending whether the player (or) computer has won (or) lost.
+class GameOver
+  using ColoredText
+
+  def initialize(count)
+    @count = count
+  end
+
+  def player_won(guess_log, score_log)
+    PrintWinner.player(@count)
+    PrintLogs(guess_log, score_log)
+  end
+
+  def player_lost(secret_code, guess_log, score_log)
+    PrintLoser.player(@count)
+    PrintLogs(guess_log, score_log)
+    puts ''
+    puts 'The right combination is:- '.color_it('bright_green')
+    puts ''
+    PrintAnswer.show(secret_code)
+  end
+
+  def computer_won(guess_log, score_log)
+    PrintWinner.computer(@count)
+    PrintLogs(guess_log, score_log)
+  end
+
+  def error_occured
+    PrintLoser.computer
+  end
+end
