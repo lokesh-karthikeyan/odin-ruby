5a1de5cc817753a3ee56b45fa6e6338cdd273c7c
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-04 22:32:07 +0530

    Add `class: PrintScore` to print scores
     + It handles the printing of the score, which was computed from player's guesses.
    
    For every guess, feedback will be printed with neat looking borders & colors.

---
+++ mastermind/lib/player_makes_guess/print_score.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative 'colored_text'
+
+# This Class prints out the feedback / score of entered guess.
+class PrintScore
+  class << self
+    using ColoredText
+
+    def show(score)
+      puts ''
+      puts '+----------•----------+'.color_it('cyan')
+      print '|'.color_it('cyan')
+      score.each { |score_icon| print "  #{score_icon}  " }
+      puts ' |'.color_it('cyan')
+      puts '+----------•----------+'.color_it('cyan')
+      puts ''
+    end
+  end
+end
