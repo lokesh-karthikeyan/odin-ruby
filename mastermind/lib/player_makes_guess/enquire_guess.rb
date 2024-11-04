cc867f119429048ac0610abc6136a3fa39c37c23
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-04 14:36:38 +0530

    Add `class: EnquireGuess` to ask guesses
     + This class contains the instance methods which will ask the players to make a guess in "," comma separated format.
     + Until the valid guesses are provided, it will keep on asking.
    
    Now, whenever the invalid guess is entered, it prints an error message & asks to enter input again.

---
+++ mastermind/lib/player_makes_guess/enquire_guess.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require_relative '../algorithms/possibilities'
+require_relative '../color_codes'
+require_relative '../colored_text'
+
+# This Class has methods to ask a guess from a player.
+class EnquireGuess
+  private
+
+  include ColorCodes
+  using ColoredText
+
+  def initialize
+    @possibilities = Possibilities.all_possibilities
+  end
+
+  def valid_guess?(guess)
+    @possibilities.include?(guess)
+  end
+
+  def to_color_codes(guess)
+    guess_of_colors = guess.tr(' ', '').downcase.split(',')
+    guess_of_colors.map! do |color|
+      color_of_a_code[color]
+    end
+    guess_of_colors.join
+  end
+
+  def print_enter_guess(guess, max_guess)
+    print "Enter your guess (#{guess}/#{max_guess}): ".color_it('light_cyan')
+    to_color_codes(gets.chomp)
+  end
+
+  def print_invalid_guess(guess, max_guess)
+    puts 'Invalid guess!!! Make a valid guess as per the format with the available colors.'.color_it('red')
+    puts ''
+    print_enter_guess(guess, max_guess)
+  end
+
+  public
+
+  def enter_guess(guess, max_guess)
+    guess_code = print_enter_guess(guess, max_guess)
+    guess_code = print_invalid_guess(guess, max_guess) until valid_guess?(guess_code)
+    guess_code
+  end
+end
