171c8d59e2449444148cb96916f098b5ab515435
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-14 14:35:35 +0530

    Add `clean_zipcode()` method to get valid code
     + It gets the "zipcode" as an argument & modifies accordingly.
    
    Now, the missed out zipcodes (or) lesser length (or) greater length zipcodes are modified.
    Maximum length of zipcode is 5, so exceeding this length are trimmed & '0' is added if the length is lower or 'nil'.

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -2,6 +2,18 @@
 
 require 'csv'
 
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    '00000'
+  elsif zipcode.length < 5
+    zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
 puts 'Event Manager Initialized!'
 puts ''
 
@@ -9,6 +21,7 @@ contents = CSV.open('event_attendees.csv', headers: true, header_converters: :sy
 
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
+  zipcode = clean_zipcode(row[:zipcode])
+
   puts "#{name} lives in \"#{zipcode}\""
 end
