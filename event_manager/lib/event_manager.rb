36d4ca7538ac4baf7cb1908d83c482203358d200
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-16 14:18:42 +0530

    Refactor the code base through `modularization`
     + Moved the working component of each data extraction to a different module to handle that data.
     + Created a "EventManager" class & passes the necessary data to other modules.
    
    Now, this class only handles of calling other modules & passing the necessary arguments to those modules.

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,103 +1,74 @@
 # frozen_string_literal: true
 
-require 'csv'
-require 'google/apis/civicinfo_v2'
-require 'erb'
+require_relative 'clean_data'
+require_relative 'thank_you_letter/thank_you_letter'
+require_relative 'attendee_phone_number/attendee_phone_number'
+require_relative 'peak_hours/peak_hours'
+require_relative 'peak_days/peak_days'
 
-def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, '0')[0..4]
-end
+# This Class manages the CSV file & passes the CSV file data to other classes.
+class EventManager
+  private
 
-def access_api_data
-  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-  civic_info.key = File.read('secret.key').strip
-  civic_info
-end
+  attr_accessor :event_manager_start, :event_manager_end
 
-def legislators_by_zipcode(zipcode)
-  civic_info = access_api_data
-  begin
-    civic_info.representative_info_by_address(address: zipcode, levels: 'country',
-                                              roles: %w[legislatorUpperBody legislatorLowerBody]).officials
-  rescue StandardError
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  include CleanData
+
+  def initialize
+    @file_contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+    @event_manager_start = 'Event Manager is now initialized.'
+    @event_manager_end = "Output is generated successfully.
+---------------------------------------------------------------------"
+  end
+
+  public
+
+  def thank_you_letter
+    puts event_manager_start
+    @file_contents.each do |row|
+      id = row[0].to_s
+      name = row[:first_name].to_s
+      zipcode = Zipcode.format_zipcode(row[:zipcode].to_s)
+      save_letter = ThankYouLetter.new(name, zipcode)
+      save_letter.generate_output(id)
+    end
+    puts event_manager_end
+  end
+
+  def attendee_phone_number
+    puts event_manager_start
+    @file_contents.each do |row|
+      id = row[0].to_s
+      name = row[:first_name].to_s
+      phone_number = PhoneNumber.format_phone_number(row[:homephone].to_s)
+      save_phone_number = AttendeePhoneNumber.new(name, phone_number)
+      save_phone_number.generate_output(id)
+    end
+    puts event_manager_end
+  end
+
+  def peak_hour
+    puts event_manager_start
+    @file_contents.each do |row|
+      reg_hours = DateAndTime.hour(row[:regdate].to_s)
+      PeakHours.new(reg_hours)
+    end
+    PeakHours.generate_output
+    puts event_manager_end
+  end
+
+  def peak_day
+    puts event_manager_start
+    @file_contents.each do |row|
+      reg_days = DateAndTime.wday(row[:regdate].to_s)
+      PeakDays.new(reg_days)
+    end
+    PeakDays.generate_output
+    puts event_manager_end
   end
 end
 
-def save_thank_you_letter(id, form_letter)
-  Dir.mkdir('output') unless Dir.exist?('output')
-
-  filename = "output/thanks_#{id}.html"
-
-  File.open(filename, 'w') do |file|
-    file.puts form_letter
-  end
-end
-
-def clean_phone_number(phone_number)
-  phone_number = phone_number.to_s.delete('^0-9')
-  phone_number = if bad_number?(phone_number)
-                   'X' * 10
-                 elsif phone_number.length == 11 && phone_number[0] == '1'
-                   phone_number[1..10]
-                 else
-                   phone_number
-                 end
-
-  "#{phone_number[0..2]}-#{phone_number[3..5]}-#{phone_number[6..]}"
-end
-
-def bad_number?(number)
-  number.length < 10 || number.length > 11 || (number.length == 11 && number[0] != '1')
-end
-
-def registered_hour(date_time)
-  date_time = DateTime.strptime(date_time, '%m/%d/%y %k:%M')
-  date_time.hour
-end
-
-def peak_hour(reg_hour)
-  peak_time = reg_hour.select { |_hour, count| count == reg_hour.values.max }.keys
-  peak_time.join(', ')
-end
-
-def registered_day(date_time)
-  date_time = DateTime.strptime(date_time, '%m/%d/%y %k:%M')
-  date_time.wday
-end
-
-def peak_day(reg_wday)
-  peak_wday = reg_wday.select { |_hour, count| count == reg_wday.values.max }.keys
-  peak_day_of_the_week = []
-  peak_wday.each do |day|
-    peak_day_of_the_week << Date::DAYNAMES[day.to_i]
-  end
-  peak_day_of_the_week.join(', ')
-end
-
-puts 'Event Manager Initialized!'
-
-contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
-
-# template_letter = File.read('form_letter.erb')
-# erb_template = ERB.new template_letter
-
-reg_hour = Hash.new(0)
-reg_wday = Hash.new(0)
-
-contents.each do |row|
-  # id = row[0]
-  # name = row[:first_name]
-  # zipcode = clean_zipcode(row[:zipcode])
-  # legislators = legislators_by_zipcode(zipcode)
-  # form_letter = erb_template.result(binding)
-  # save_thank_you_letter(id, form_letter)
-  # phone_number = clean_phone_number(row[:homephone])
-  # puts "#{name} : #{phone_number} || #{phone_number.length}"
-  reg_date = row[:regdate]
-  reg_hour[registered_hour(reg_date)] += 1
-  reg_wday[registered_day(reg_date)] += 1
-end
-
-p peak_hour(reg_hour)
-p peak_day(reg_wday)
+EventManager.new.thank_you_letter
+EventManager.new.attendee_phone_number
+EventManager.new.peak_hour
+EventManager.new.peak_day
