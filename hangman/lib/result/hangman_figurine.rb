dbb615b0a4e4fa942870fa2f94e90ac0161d7fc2
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-22 13:52:28 +0530

    Add `module -> HangmanFigurine` to print figures
     + This module contains a figure of "Stick man" developing with head, hands, legs & at last gets hanged.
     + For each incorrect guess, an appropriate "Hangman" figurine is printed.
     + The "print_figurine()" method takes the current lives as a parameter & concatenates with "figurine" string.
     + Then the method name is passed as an argument to "send()" method, due to the method name is in the format of string.
    
    Now, when the player makes an incorrect guess, an hangman figurine is slowly created & hanged.
    It adds visualization for the remaining lives message.

---
+++ hangman/lib/result/hangman_figurine.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require_relative '../colorable/colorable'
+
+# This Module contains the hangman figurine of various designs.
+module HangmanFigurine
+  using Colorable
+
+  def print_figurine(lives)
+    method = "figurine#{lives}"
+    send(method)
+  end
+
+  def figurine_header
+    puts ''
+    puts '      +------+'.color(:steel_blue)
+    puts '      |      |'.color(:steel_blue)
+  end
+
+  def figurine_footer(color = :steel_blue)
+    puts '             |'.color(:steel_blue)
+    puts '  ==============='.color(color)
+  end
+
+  def figurine_face
+    puts '      O'.color(:green) + '      |'.color(:steel_blue)
+  end
+
+  def figurine_body_and_hands
+    puts '     /|\\'.color(:green) + '     |'.color(:steel_blue)
+  end
+
+  def figurine_full_body(color = :green)
+    puts '      O'.color(color) + '      |'.color(:steel_blue)
+    puts '     /|\\'.color(color) + '     |'.color(:steel_blue)
+    puts '     / \\'.color(color) + '     |'.color(:steel_blue)
+  end
+
+  def figurine7
+    figurine_header
+    puts '             |'.color(:steel_blue)
+    puts '             |'.color(:steel_blue)
+    puts '             |'.color(:steel_blue)
+    figurine_footer
+  end
+
+  def figurine6
+    figurine_header
+    figurine_face
+    puts '             |'.color(:steel_blue)
+    puts '             |'.color(:steel_blue)
+    figurine_footer
+  end
+
+  def figurine5
+    figurine_header
+    figurine_face
+    puts '      |'.color(:green) + '      |'.color(:steel_blue)
+    puts '             |'.color(:steel_blue)
+    figurine_footer
+  end
+
+  def figurine4
+    figurine_header
+    figurine_face
+    puts '     /|'.color(:green) + '      |'.color(:steel_blue)
+    puts '             |'.color(:steel_blue)
+    figurine_footer
+  end
+
+  def figurine3
+    figurine_header
+    figurine_face
+    figurine_body_and_hands
+    puts '             |'.color(:steel_blue)
+    figurine_footer
+  end
+
+  def figurine2
+    figurine_header
+    figurine_face
+    figurine_body_and_hands
+    puts '     /'.color(:green) + '       |'.color(:steel_blue)
+    figurine_footer
+  end
+
+  def figurine1
+    figurine_header
+    figurine_face
+    figurine_body_and_hands
+    puts '     / \\'.color(:green) + '     |'.color(:steel_blue)
+    figurine_footer
+  end
+
+  def figurine0
+    figurine_header
+    figurine_full_body(:red)
+    figurine_footer(:red)
+  end
+end
