e122b7173e4fae120ed509038d0be7985a8553e8
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-29 17:40:14 +0530

    Add `Fibonacci#fibs_rec` method
     + It generates the "Fibonacci Sequence" of 'n' numbers.
     + The total count depends on the arguments passed.
     + If the argument is other than '0', it provides the 'fibonacci sequence' of argument's value.
     + It uses "Recursion" to achieve the sequence.

--- recursion/lib/fibonacci.rb
+++ recursion/lib/fibonacci.rb
@@ -13,6 +13,21 @@ class Fibonacci
     end
     puts "The Fibonacci Sequence of #{number} numbers are => #{fibonacci_sequence}"
   end
+
+  def fibs_rec(number, initial_value = 0, fibonacci_sequence = [])
+    return if number.zero?
+
+    statement = ->(count, fib_sequence) { puts "The Fibonacci Sequence of #{count} number(s) are => #{fib_sequence}" }
+    return statement.call(number, fibonacci_sequence) if number.eql?(initial_value)
+
+    fibonacci_sequence << initial_value if initial_value < 2
+    if initial_value >= 2
+      fibonacci_sequence << (fibonacci_sequence[initial_value - 1] + fibonacci_sequence[initial_value - 2])
+    end
+
+    fibs_rec(number, initial_value + 1, fibonacci_sequence)
+  end
 end
 
-Fibonacci.new.fibs(8)
+# Fibonacci.new.fibs(0)
+Fibonacci.new.fibs_rec(8)
