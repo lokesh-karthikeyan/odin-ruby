ffb28e95b34378d862e21dcba07927684301c774
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-11-29 18:22:40 +0530

    Fix `Fibonacci#fibs` if "argument==0"
     + Modified the code, so it returns if the paramater is '0'.
     + Used 'return' statement instead of 'break' and utilized 'lambda' for printing sequence.
    
    Before Change ==> When argument is '0', it prints a valid sequence (0th index).
    After Change ==> When argument is '0', it returns nothing.

--- recursion/lib/fibonacci.rb
+++ recursion/lib/fibonacci.rb
@@ -3,21 +3,24 @@
 # This Class generates 'Fibonacci Sequence' in an array with 'N' elements.
 class Fibonacci
   def fibs(number)
+    return if number.zero?
+
     fibonacci_sequence = []
+    statement = ->(count, fib_sequence) { puts "The Fibonacci Sequence of #{count} number(s) are => #{fib_sequence}" }
+
     0.upto(number) do |count|
-      next fibonacci_sequence << count if count.zero? || count.eql?(1)
+      return statement.call(number, fibonacci_sequence) if fibonacci_sequence.length.eql?(number)
 
-      break if fibonacci_sequence.length.eql?(number)
-
-      fibonacci_sequence << (fibonacci_sequence[count - 1] + fibonacci_sequence[count - 2])
+      fibonacci_sequence << count if count < 2
+      fibonacci_sequence << (fibonacci_sequence[count - 1] + fibonacci_sequence[count - 2]) if count >= 2
     end
-    puts "The Fibonacci Sequence of #{number} numbers are => #{fibonacci_sequence}"
   end
 
   def fibs_rec(number, initial_value = 0, fibonacci_sequence = [])
     return if number.zero?
 
     statement = ->(count, fib_sequence) { puts "The Fibonacci Sequence of #{count} number(s) are => #{fib_sequence}" }
+
     return statement.call(number, fibonacci_sequence) if number.eql?(initial_value)
 
     fibonacci_sequence << initial_value if initial_value < 2
@@ -29,5 +32,5 @@ class Fibonacci
   end
 end
 
-# Fibonacci.new.fibs(0)
+Fibonacci.new.fibs(8)
 Fibonacci.new.fibs_rec(8)
