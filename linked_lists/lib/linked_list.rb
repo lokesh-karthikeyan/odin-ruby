e7640b97ee64390c5d2c033f6d102ad891064f14
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-10 15:12:35 +0530

    Add `LinkedList#append` & `#to_s` methods
     + The "append()" method adds the given value to list at the end.
     + The "to_s()" method overrides the default "to_s()" behaviour of this class.
    
    Now, new values can be appended to the list.
    Whenever the instantiated object is called without any methods, it shows the linked list.

---
+++ linked_lists/lib/linked_list.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: false
+
+require_relative 'node'
+
+# This Class manages the 'Linked List' data structure & performs list related operations.
+class LinkedList
+  private
+
+  attr_accessor :head, :tail, :node
+
+  def initialize(node = Node)
+    self.node = node
+    self.head = nil
+    self.tail = nil
+  end
+
+  def to_s
+    current_node = @head
+    list = ''
+    until current_node.nil?
+      list << "( #{current_node.value} ) -> "
+      current_node = current_node.next_node
+    end
+    list << 'nil'
+  end
+
+  public
+
+  def append(value)
+    new_node = node.new(value)
+
+    unless tail.nil?
+      @tail.next_node = new_node
+      @tail = new_node
+      return
+    end
+
+    @head = new_node
+    @tail = new_node
+  end
+end
+
+list = LinkedList.new
+
+list.append('dog')
+list.append('cat')
+
+puts list
