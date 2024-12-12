68eadc925b9247489ff7e4b9ffe7b8395a1df5d8
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-12 19:39:45 +0530

    Drop `HashCode#initialize` method
     + Simplified the class by removing the "initialize()" method.

--- hash_map/lib/hash_code.rb
+++ hash_map/lib/hash_code.rb
@@ -2,19 +2,11 @@
 
 # This Class creates hash from the passed in string as an argument.
 class HashCode
-  private
-
   PRIME_NUMBER = 31
-  attr_accessor :key
 
-  def initialize(key = nil)
-    self.key = key
-  end
-
-  public
-
-  def generate
+  def generate(key)
     hash_code = 0
+
     key.each_char { |char| hash_code = PRIME_NUMBER * hash_code + char.ord }
     hash_code
   end
