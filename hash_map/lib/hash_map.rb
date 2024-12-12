de901f2e77474071ecdc5955ffa74d54823bba6e
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-12 22:53:30 +0530

    Add `HashMap#set` method
     + It takes in a key, value pair and stores as a linked list.
     + It generates a "hash_code" from the key & stores the value in a proper "hash_code" index.
     + Raises an "Index" error if it generates an invalid index.
    
    Before Change ==> Data can't be stored in a hash map.
    After Change ==> Data can be stored in a hash map.

--- hash_map/lib/hash_map.rb
+++ hash_map/lib/hash_map.rb
@@ -8,16 +8,46 @@ require_relative 'linked_list/linked_list'
 class HashMap
   private
 
+  LOAD_FACTOR = 0.75
+
   attr_accessor :hash_code, :bucket_stash, :bucket, :list
-  attr_reader :load_factor
 
   def initialize(hash_code = HashCode.new, bucket_stash = Buckets.new, list = LinkedList.new)
     self.hash_code = hash_code
     self.bucket_stash = bucket_stash
     self.bucket = bucket_stash.store
     self.list = list
-    self.load_factor = 0.75
+    self.list.pointer = bucket
+  end
+
+  def compute_index(key)
+    key = hash_code.generate(key)
+    key % 16
+  end
+
+  def invalid_index?(index) = index.negative? || index >= bucket.length
+
+  public
+
+  def set(key, value)
+    index = compute_index(key)
+    raise IndexError if invalid_index?(index)
+
+    list.add(index, key, value)
   end
 end
 
 my_hash = HashMap.new
+
+my_hash.set('apple', 'red')
+my_hash.set('banana', 'yellow')
+my_hash.set('carrot', 'orange')
+my_hash.set('dog', 'brown')
+my_hash.set('elephant', 'gray')
+my_hash.set('frog', 'green')
+my_hash.set('grape', 'purple')
+my_hash.set('hat', 'black')
+my_hash.set('ice cream', 'white')
+my_hash.set('jacket', 'blue')
+my_hash.set('kite', 'pink')
+my_hash.set('lion', 'golden')
