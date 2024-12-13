c372c9c93199f000035f80a7e587e8e9f7c63f47
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-14 00:42:29 +0530

    Add `Dynamic size` for the `HashMap`
     + "load_levels()" -> Computes the maximum entries allowed in the existing size.
     + "increase_buckets()" -> Doubles the size of the total buckets.
    
    Before Change ==> There's no way to increase the number of buckets.
    After Change ==> Based on the total entries, buckets will be doubled after reaching "load_levels()".

--- hash_map/lib/hash_map.rb
+++ hash_map/lib/hash_map.rb
@@ -28,6 +28,13 @@ class HashMap
 
   def decrease_length = self.length -= 1
 
+  def load_levels = bucket.size * LOAD_FACTOR
+
+  def increase_buckets
+    bucket_stash.resize
+    self.bucket = bucket_stash.store
+  end
+
   public
 
   attr_accessor :length
@@ -37,6 +44,7 @@ class HashMap
     raise IndexError if invalid_index?(index)
 
     add(index, key, value)
+    increase_buckets if length > load_levels
   end
 
   def get(key)
@@ -94,43 +102,67 @@ my_hash.set('jacket', 'blue')
 my_hash.set('kite', 'pink')
 my_hash.set('lion', 'golden')
 
-p my_hash.remove('kitee')
-p my_hash.remove('kite')
-
-p my_hash.get('lionn')
-p my_hash.get('carrot')
-
-p my_hash.has?('carrot')
-p my_hash.has?('bear')
-
-p my_hash.length
-
-p my_hash.entries
-
-my_hash.clear
-
-p my_hash.entries
-
-p my_hash.length
-
-my_hash.set('apple', 'red')
-my_hash.set('banana', 'yellow')
-my_hash.set('carrot', 'orange')
-my_hash.set('dog', 'brown')
-my_hash.set('elephant', 'gray')
-my_hash.set('frog', 'green')
-my_hash.set('grape', 'purple')
-my_hash.set('hat', 'black')
-my_hash.set('ice cream', 'white')
-my_hash.set('jacket', 'blue')
-my_hash.set('kite', 'pink')
-my_hash.set('lion', 'golden')
-p my_hash.length
-
+p "Entries = #{my_hash.length}"
 # p my_hash.entries
-p my_hash.keys
-p my_hash.values
 
-my_hash.clear
+my_hash.set('hat', 'black')
+my_hash.set('lion', 'golden')
+my_hash.set('hat', 'black')
+my_hash.set('lion', 'golden')
+my_hash.set('hat', 'black')
+my_hash.set('lion', 'golden')
+my_hash.set('hat', 'black')
+my_hash.set('lion', 'golden')
+my_hash.set('hat', 'black')
+my_hash.set('lion', 'golden')
+my_hash.set('hat', 'black')
+my_hash.set('lion', 'golden')
+my_hash.set('hat', 'black')
+my_hash.set('lion', 'golden')
+my_hash.set('kite', 'pinkkkkkkkkkkkkkk')
+my_hash.set('jacket', 'black')
 
-p my_hash.entries
+p "Entries = #{my_hash.length}"
+# p my_hash.entries
+
+# p my_hash.has?('lion')
+# p my_hash.remove('kitee')
+# p my_hash.remove('kite')
+#
+# p my_hash.get('lionn')
+# p my_hash.get('carrot')
+#
+# p my_hash.has?('carrot')
+# p my_hash.has?('bear')
+#
+# p my_hash.length
+#
+# p my_hash.entries
+#
+# my_hash.clear
+#
+# p my_hash.entries
+#
+# p my_hash.length
+#
+# my_hash.set('apple', 'red')
+# my_hash.set('banana', 'yellow')
+# my_hash.set('carrot', 'orange')
+# my_hash.set('dog', 'brown')
+# my_hash.set('elephant', 'gray')
+# my_hash.set('frog', 'green')
+# my_hash.set('grape', 'purple')
+# my_hash.set('hat', 'black')
+# my_hash.set('ice cream', 'white')
+# my_hash.set('jacket', 'blue')
+# my_hash.set('kite', 'pink')
+# my_hash.set('lion', 'golden')
+# p my_hash.length
+#
+# # p my_hash.entries
+# p my_hash.keys
+# p my_hash.values
+#
+# my_hash.clear
+#
+# p my_hash.entries
