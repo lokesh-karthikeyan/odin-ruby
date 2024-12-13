29ff7dedae9521d743ac9fac70c188936662189e
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-13 22:28:49 +0530

    Make `module -> HashIndexable`
     + This module is responsible for generating "hash" & computing "index".
    
    Earlier, "compute_index()" is in separate file. So, placed the related items in same module.

--- hash_map/lib/hash_code.rb
+++ hash_map/lib/hash_code.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
-# This Class creates hash from the passed in string as an argument.
-class HashCode
-  PRIME_NUMBER = 31
+# This Module contains the Class to generate 'Hash' & the method to compute 'Low number Index'.
+module HashIndexable
+  # This Class creates hash from the passed in string as an argument.
+  class HashCode
+    PRIME_NUMBER = 31
 
-  def generate(key)
-    hash_code = 0
+    def generate(key)
+      hash_code = 0
 
-    key.each_char { |char| hash_code = PRIME_NUMBER * hash_code + char.ord }
-    hash_code
+      key.each_char { |char| hash_code = PRIME_NUMBER * hash_code + char.ord }
+      hash_code
+    end
   end
+
+  DIVISOR = 16
+
+  def compute_index(key) = HashCode.generate(key) % DIVISOR
 end
