c03bf87ac67db6a3bde2c5278eb843aaa2c79a78
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-13 15:01:45 +0530

    Refactor `Class` -> `Module`
     + Changed the "LinkedList" Class to Module.
     + The "Node" class is now part of the "LinkedList" module.
    
    These changes organized the structure of the linked list in single module.

--- hash_map/lib/linked_list.rb
+++ hash_map/lib/linked_list.rb
@@ -1,26 +1,27 @@
 # frozen_string_literal: true
 
-require_relative 'node'
-
-# This Class creates an instance of a 'Linked List' data structure.
-# It also contains methods which performs list related operations.
-class LinkedList
+# This Module contains the creation of 'Linked List' Data structure.
+# It also contains linked list related operations.
+module LinkedList
   private
 
-  attr_accessor :tail
+  # This Class creates an instance of nodes.
+  # It contains 'Key', 'Value' and pointer to the 'Next node'.
+  class Node
+    def initialize(key = nil, value = nil, next_node = nil)
+      self.key = key
+      self.value = value
+      self.next_node = next_node
+    end
 
-  def initialize(node = Node)
-    @node = node
-    self.tail = []
+    attr_accessor :key, :value, :next_node
   end
 
-  def create_node(key = nil, value = nil, next_node = nil)
-    @node.new(key, value, next_node)
-  end
+  def create_node(key = nil, value = nil, next_node = nil) = Node.new(key, value, next_node)
 
   def append_first_node(index, key, value)
     new_node = create_node(key, value)
-    pointer[index] = new_node
+    bucket[index] = new_node
     tail[index] = new_node
   end
 
@@ -30,37 +31,22 @@ class LinkedList
     tail[index] = new_node
   end
 
+  def replace_node_value(index, key, value)
+    current_node = bucket[index]
+
+    while current_node
+      return current_node.value = value if current_node.key == key
+
+      current_node = current_node.next_node
+    end
+  end
+
   public
 
-  attr_accessor :pointer
-
   def add(index, key, value)
-    return append_first_node(index, key, value) if pointer[index].nil?
+    return append_first_node(index, key, value) if bucket[index].nil?
+    return replace_node_value(index, key, value) if has?(key)
 
-    append_node(index, key, value) if pointer[index]
-  end
-
-  def pull_value(index, key)
-    current_node = pointer[index]
-
-    until current_node.nil?
-      return current_node.value if current_node.key == key
-
-      current_node = current_node.next_node
-    end
-
-    'nil'
-  end
-
-  def valid_key?(index, key)
-    current_node = pointer[index]
-
-    until current_node.nil?
-      return true if current_node.key == key
-
-      current_node = current_node.next_node
-    end
-
-    false
+    append_node(index, key, value)
   end
 end
