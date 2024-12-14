d15c77f48733c4ab64c91d8995840f94ba0ddee7
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-14 17:24:43 +0530

    Add `HashMap#set` method
     + It takes "key" & "value" as an argument & returns the [key, value] pairs.
     + It computes the index & places the key, value pairs in the "bucket[index]".
     + Raises an "IndexError" if it's an invalid index.
    
    Before Change ==> Unable to add any data to this "HashMap".
    After Change ==> Can able to add "key", "value" to this "HashMap".

--- hash_map/lib/hash_map.rb
+++ hash_map/lib/hash_map.rb
@@ -3,6 +3,7 @@
 require_relative 'hash_code/hash_code'
 require_relative 'buckets/buckets'
 require_relative 'buckets/bucket_overseer'
+require_relative 'map_operations/map_operations'
 
 # This Class creates an 'Hash Map' data structure.
 # It contains methods that are related to 'Hash Map' related operations.
@@ -10,14 +11,14 @@ class HashMap
   private
 
   include BucketOverseer
+  include MapOperations
 
-  attr_accessor :hash_code, :buckets, :bucket, :tail, :length
+  attr_accessor :hash_code, :buckets, :bucket
 
   def initialize(hash_code = HashCode, buckets = Buckets)
     self.hash_code = hash_code
     self.buckets = buckets
     self.bucket = buckets.new
-    self.tail = []
     self.length = 0
   end
 
@@ -30,4 +31,23 @@ class HashMap
   def hash_index(key) = hash(key) % capacity
 
   def invalid_index?(index) = index.negative? || index >= capacity
+
+  public
+
+  attr_accessor :length
+
+  def set(key, value)
+    index = hash_index(key)
+    raise IndexError if invalid_index?(index)
+
+    increase_buckets if load_exceeded?
+    add(index, key, value)
+  end
 end
+
+hash = HashMap.new
+
+p hash.set('apple', 'red')
+p hash.set('banana', 'yellow')
+p hash.set('banana', 'yyellow')
+p hash.length
