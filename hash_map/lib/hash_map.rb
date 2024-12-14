2addd2d8f9be56694d00d392d884dba9e0016ed2
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-14 15:58:20 +0530

    Add Class -> `HashMap` & `HashSet`
     + Declared the instance variables & the methods to compute index.
     + It also has helper methods to resize "@length" variable.

---
+++ hash_map/lib/hash_map.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative 'hash_code/hash_code'
+require_relative 'buckets/buckets'
+require_relative 'buckets/bucket_overseer'
+
+# This Class creates an 'Hash Map' data structure.
+# It contains methods that are related to 'Hash Map' related operations.
+class HashMap
+  private
+
+  include BucketOverseer
+
+  attr_accessor :hash_code, :buckets, :bucket, :tail, :length
+
+  def initialize(hash_code = HashCode, buckets = Buckets)
+    self.hash_code = hash_code
+    self.buckets = buckets
+    self.bucket = buckets.new
+    self.tail = []
+    self.length = 0
+  end
+
+  def increase_length = self.length += 1
+
+  def decrease_length = self.length -= 1
+
+  def hash(key) = hash_code.generate(key)
+
+  def hash_index(key) = hash(key) % capacity
+
+  def invalid_index?(index) = index.negative? || index >= capacity
+end
