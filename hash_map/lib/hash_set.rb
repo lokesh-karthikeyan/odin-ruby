c8612f41ca32e13ac24fd9ddc94e516e910a7274
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-14 19:31:32 +0530

    Add `HashSet#set` method
     + It takes a key as an argument and returns the inserted key.
     + It computes the index & places the key in the appropriate index.
     + Returns an "IndexError" if it's an invalid index.
    
    Before Change ==> Data can't be saved inside the "Hash Set".
    After Change ==> Data can be saved inside the "Hash Set".

--- hash_map/lib/hash_set.rb
+++ hash_map/lib/hash_set.rb
@@ -3,6 +3,7 @@
 require_relative 'hash_code/hash_code'
 require_relative 'buckets/buckets'
 require_relative 'buckets/bucket_overseer'
+require_relative 'set_operations/set_operations'
 
 # This Class creates an 'Hash Set' data structure.
 # It contains methods that are related to 'Hash Set' related operations.
@@ -10,14 +11,14 @@ class HashSet
   private
 
   include BucketOverseer
+  include SetOperations
 
-  attr_accessor :hash_code, :buckets, :bucket, :tail, :length
+  attr_accessor :hash_code, :buckets, :bucket
 
   def initialize(hash_code = HashCode, buckets = Buckets)
     self.hash_code = hash_code
     self.buckets = buckets
     self.bucket = buckets.new
-    self.tail = []
     self.length = 0
   end
 
@@ -30,4 +31,23 @@ class HashSet
   def hash_index(key) = hash(key) % capacity
 
   def invalid_index?(index) = index.negative? || index >= capacity
+
+  public
+
+  attr_accessor :length
+
+  def set(key)
+    index = hash_index(key)
+    raise IndexError if invalid_index?(index)
+
+    increase_buckets if load_exceeded?
+    add(index, key)
+  end
 end
+
+hash_set = HashSet.new
+
+p hash_set.set('apple')
+p hash_set.set('banana')
+p hash_set.set('banana')
+p hash_set.length
