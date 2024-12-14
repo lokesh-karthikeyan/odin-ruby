34cd23a2b3188b99175c0ad24fb4aa55e162642a
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-14 18:07:53 +0530

    Add `#add, #create_node, #insert_node, #append_node`
     + "add()" -> Checks where to add the new node.
     + "create_node()" -> It creates a new node from the "Node" class.
     + "insert_node()" -> It inserts the new node, if the index is empty.
     + "append_node()" -> It appends the new node, to the existing node.
    
    These helper methods helps to perform the new node insertion into the hash map.

--- hash_map/lib/map_operations/map_operations.rb
+++ hash_map/lib/map_operations/map_operations.rb
@@ -3,6 +3,8 @@
 # This Module contains 'Node' Class for 'Hash Map'.
 # It also has methods that are related to 'Hash Map'.
 module MapOperations
+  private
+
   # This Class creates 'Node' data structure for 'Hash Map'.
   class Node
     attr_accessor :key, :value, :next_node
@@ -12,5 +14,29 @@ module MapOperations
       self.value = value
       self.next_node = next_node
     end
+  end
+
+  def create_node(key, value, next_node = nil) = Node.new(key, value, next_node)
+
+  def insert_node(index, key, value)
+    new_node = create_node(key, value)
+    bucket[index] = new_node
+    increase_length
+    [new_node.key, new_node.value]
+  end
+
+  def append_node(index, key, value)
+    new_node = create_node(key, value)
+    current_node = bucket[index]
+    current_node = current_node.next_node while current_node.next_node
+    current_node.next_node = new_node
+    increase_length
+    [new_node.key, new_node.value]
+  end
+
+  def add(index, key, value)
+    return append_node(index, key, value) if bucket[index]
+
+    insert_node(index, key, value)
   end
 end
