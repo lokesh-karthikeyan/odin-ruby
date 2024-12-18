0ebc8557feda90e7a6be7a1d9f97cc9397e1b9ce
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-18 20:14:49 +0530

    Add `Tree#insert_node` method
     + It takes in a given value & inserts it into the tree structure.
     + If there is no tree, it creates a new instance & stores it in the current root -> "insert_first_node()".
     + If value exists, it just returns without inserting.
     + "insert_as_leaf_node()" -> It inserts the value at a proper position in the tree structure.
    
    Before Change ==> Tree can only be grown from scratch, no way to insert a node.
    After Change ==> A value can be inserted into a Tree structure as a node.

--- binary_search_tree/lib/tree/tree.rb
+++ binary_search_tree/lib/tree/tree.rb
@@ -12,6 +12,21 @@ class Tree
     view(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true) if node.left
   end
 
+  def insert_node(value, previous_node = nil, root_node = @root)
+    return insert_first_node(value) if root_node.nil?
+
+    while root_node
+      return value if value == root_node.value
+
+      previous_node = root_node
+      root_node = value < root_node.value ? root_node.left : root_node.right
+    end
+
+    return insert_as_leaf_node(value, previous_node, is_left: true) if value < previous_node.value
+
+    insert_as_leaf_node(value, previous_node, is_left: false)
+  end
+
   private
 
   attr_accessor :node
@@ -33,4 +48,18 @@ class Tree
   end
 
   def create_node(value = nil, left = nil, right = nil) = node.new(value, left, right)
+
+  def insert_first_node(value)
+    self.root = self.class.new([value]).root
+    value
+  end
+
+  def insert_as_leaf_node(value, node, is_left: true)
+    unless is_left
+      node.right = create_node(value)
+      return value
+    end
+    node.left = create_node(value)
+    value
+  end
 end
