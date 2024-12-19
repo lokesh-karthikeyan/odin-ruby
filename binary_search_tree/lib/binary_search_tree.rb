3f381143e700f524203aceb81d6427a287e8b297
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-19 18:52:50 +0530

    Add `BinarySearchTree#insert` method
     + It takes in a value & fetches the "parent" and "child" node -> "find_family()".
     + If there is a child node, then the value must exist in the tree. Returns value.
     + If both the "parent" and "child" are "nil", then there is no active tree, inserts the value as root -> "create_tree()".
     + If the "child" alone is "nil", it inserts the new node to the previous node's left (or) right.
    
    Before Change ==> Insertion methods are placed inside the "Tree" class.
    After Change ==> Only the "insert_as_leaf_node()" is still placed in the "Tree" class.

--- binary_search_tree/lib/binary_search_tree.rb
+++ binary_search_tree/lib/binary_search_tree.rb
@@ -12,15 +12,41 @@ class BinarySearchTree
     view(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true) if node.left
   end
 
+  def insert(value)
+    previous_node, current_node = find_family(value, @root)
+
+    return value if current_node
+    return create_tree([value]) if previous_node.nil? && current_node.nil?
+    return binary_search_tree.insert_as_leaf_node(value, previous_node, is_left: true) if value < previous_node.value
+
+    binary_search_tree.insert_as_leaf_node(value, previous_node, is_left: false)
+  end
+
   private
 
-  attr_accessor :tree, :root, :traverse
+  attr_accessor :tree, :root, :traverse, :binary_search_tree
 
   def initialize(array, tree = Tree, traverse = Traverse.new, quick_sort = QuickSort)
     remove_duplicates = array.uniq
     sorted_array = quick_sort.new(remove_duplicates).sorted_array
     self.traverse = traverse
-    self.tree = tree.new(sorted_array)
-    self.root = self.tree.root
+    self.tree = tree
+    create_tree(sorted_array)
+  end
+
+  def create_tree(array)
+    self.binary_search_tree = tree.new(array)
+    self.root = binary_search_tree.root
+    root&.value
+  end
+
+  def find_family(value, current_node, previous_node = nil)
+    while current_node
+      return [previous_node, current_node] if value == current_node.value
+
+      previous_node = current_node
+      current_node = value < current_node.value ? current_node.left : current_node.right
+    end
+    [previous_node, current_node]
   end
 end
