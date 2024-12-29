9ffdefd1786b11724e8cc2138c09fa38c50020f7
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-12-29 15:10:44 +0530

    Refactor `#caesar_cipher` method
     + Removed all the complicated conditional statements.
     + Added "to_ascii()" method -> Converts the string to array of bytes.
     + Added "shift_characters()" method -> Returns the new number after shifting accordingly.
    
    Before Change ==> Too much nested conditionals.
    After Change ==> Conditionals were drastically reduced.

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,25 +1,18 @@
+# frozen_string_literal: true
+
 def caesar_cipher(sentence_to_be_encoded, shift_count)
-  in_ascii = sentence_to_be_encoded.bytes
-  shifted_characters = in_ascii.map do |character|
-    if character.between?(65, 90)
-      if character + shift_count > 90
-        shift_from_start = (character + shift_count) - 90
-        64 + shift_from_start
-      else
-        character + shift_count
-      end
-    elsif character.between?(97, 122)
-      if character + shift_count > 122
-        shift_from_start = (character + shift_count) - 122
-        96 + shift_from_start
-      else
-        character + shift_count
-      end
-    else
-      character
-    end
-  end
+  in_ascii = to_ascii(sentence_to_be_encoded)
+  shifted_characters = in_ascii.map { |char_number| shift_characters(char_number, shift_count) }
   shifted_characters.map(&:chr).join
+end
+
+def to_ascii(string) = string.bytes
+
+def shift_characters(char_number, shift_count)
+  return char_number unless char_number.between?(65, 90) || char_number.between?(97, 122)
+
+  base = char_number < 91 ? 65 : 97
+  (((char_number - base) + shift_count) % 26) + base
 end
 
 caesar_cipher('What a string!', 5)
