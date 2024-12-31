e7deaeb3a071e70285b84dd70c484ff760f6f527
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-01-01 02:22:20 +0530

    Make `Test` cases for board create & update
     + "#initialize" -> Made tests when the "GameBoard" class is instantiated.
     + "#update_board" -> Made tests when the board gets updated.
    
    All of these test cases has been successfully passed.

---
+++ connect_four/spec/game_board_spec.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+require_relative '../lib/board/game_board'
+
+# rubocop:disable Metrics/BlockLength
+describe GameBoard do
+  subject(:game_board) { described_class.new }
+  let(:board) { game_board.instance_variable_get(:@board) }
+
+  describe '#initialize' do
+    context "When it's instantiated" do
+      let(:model_board) do
+        [
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0]
+        ]
+      end
+
+      it 'should assign a matrix-array for an instance variable' do
+        expect(board).to eq(model_board)
+      end
+    end
+  end
+
+  describe '#update_board' do
+    context 'When the column is empty' do
+      let(:model_board) do
+        [
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 1, 0]
+        ]
+      end
+
+      before do
+        game_board.update_board(position, player_id)
+      end
+
+      let(:position)  { 6 }
+      let(:player_id) { 1 }
+
+      it 'should update from bottom-up row' do
+        expect(board).to eq(model_board)
+      end
+      context 'When the current selection is saved' do
+        let(:previous_move) { game_board.instance_variable_get(:@last_move) }
+
+        it 'should assign the current move to the last_move variable' do
+          current_move = [5, 5]
+          expect(previous_move).to eq(current_move)
+        end
+      end
+    end
+
+    context 'When the column is not empty' do
+      let(:model_board) do
+        [
+          [0, 0, 2, 0, 0, 0, 0],
+          [0, 0, 2, 0, 0, 0, 0],
+          [0, 0, 1, 0, 0, 0, 0],
+          [2, 0, 2, 0, 0, 0, 0],
+          [1, 0, 1, 0, 0, 1, 0],
+          [2, 0, 2, 1, 2, 1, 0]
+        ]
+      end
+
+      before do
+        set_board = [
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 2, 0, 0, 0, 0],
+          [0, 0, 1, 0, 0, 0, 0],
+          [2, 0, 2, 0, 0, 0, 0],
+          [1, 0, 1, 0, 0, 1, 0],
+          [2, 0, 2, 1, 2, 1, 0]
+        ]
+        game_board.instance_variable_set(:@board, set_board)
+        game_board.update_board(position, player_id)
+      end
+      let(:position) { 3 }
+      let(:player_id) { 2 }
+
+      it 'should skip the already marked row & moves upward (row - 1)' do
+        expect(board).to eq(model_board)
+      end
+
+      context 'When the current selection is saved' do
+        let(:previous_move) { game_board.instance_variable_get(:@last_move) }
+
+        it 'should assign the current move to the last_move variable' do
+          current_move = [0, 2]
+          expect(previous_move).to eq(current_move)
+        end
+      end
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
