5f040dd4f3daf67ba429e6557468c4e12da2a1b9
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-01-01 22:10:54 +0530

    Add `ConditionsToWin#horizontal_connection?` method
     + It returns false, if the array has no connection in an horizontal path.
     + If there is a connection in an horizontal manner, returns true.

---
+++ connect_four/lib/conditions_to_win/conditions_to_win.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# Functionalities to check if the given array has 4 connected discs.
+# Those discs must be either aligned by horizontal (or) vertical (or) diagonal.
+class ConditionsToWin
+  def conditions_satisfied?(matrix_array, indices, player_id)
+    self.matrix_array = matrix_array
+    self.indices = indices
+    self.player_id = player_id
+    horizontal_connection?
+  end
+
+  def horizontal_connection?
+    discs_count = 1
+    column = indices.last
+    row = indices.first
+    CONNECTED_DISCS.times do
+      column += 1
+      discs_count += 1 if identical_discs?(row, column)
+      break if !identical_discs?(row, column) || discs_connected?(discs_count)
+    end
+    column = indices.last
+    CONNECTED_DISCS.times do
+      column -= 1
+      discs_count += 1 if identical_discs?(row, column)
+      break if !identical_discs?(row, column) || discs_connected?(discs_count)
+    end
+    discs_connected?(discs_count)
+  end
+
+  private
+
+  CONNECTED_DISCS = 4
+
+  attr_accessor :matrix_array, :indices, :player_id
+
+  def discs_connected?(discs_count) = (discs_count == CONNECTED_DISCS)
+
+  def identical_discs?(row_index, column_index) = matrix_array[row_index][column_index] == player_id
+end
