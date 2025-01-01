44bef6ff2082f1922f0c1352fffbf94302e622c6
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-01-02 00:23:06 +0530

    Add `ConditionsToWin#leading_diagonal_connection?`
     + Leading diagonal: It starts from top left -> bottom right.
     + If there is a connection in this path, returns true. Else returns false.
    
    Before Change ==> Horizontal, Vertical checks is only available.
    After Change ==> Now, main diagonal check is also available.

--- connect_four/lib/conditions_to_win/conditions_to_win.rb
+++ connect_four/lib/conditions_to_win/conditions_to_win.rb
@@ -11,39 +11,60 @@ class ConditionsToWin
   end
 
   def horizontal_connection?
-    discs_count = 1
+    total_discs = 1
     row = indices.first
     column = indices.last
     CONNECTED_DISCS.times do
       column += 1
-      discs_count += 1 if identical_discs?(row, column)
-      break if !identical_discs?(row, column) || discs_connected?(discs_count)
+      total_discs += 1 if identical_discs?(row, column)
+      break if !identical_discs?(row, column) || discs_connected?(total_discs)
     end
     column = indices.last
     CONNECTED_DISCS.times do
       column -= 1
-      discs_count += 1 if identical_discs?(row, column)
-      break if !identical_discs?(row, column) || discs_connected?(discs_count)
+      total_discs += 1 if identical_discs?(row, column)
+      break if !identical_discs?(row, column) || discs_connected?(total_discs)
     end
-    discs_connected?(discs_count)
+    discs_connected?(total_discs)
   end
 
   def vertical_connection?
-    discs_count = 1
+    total_discs = 1
     row = indices.first
     column = indices.last
     CONNECTED_DISCS.times do
       row += 1
-      discs_count += 1 if identical_discs?(row, column)
-      break if !identical_discs?(row, column) || discs_connected?(discs_count) || last_row?(row)
+      total_discs += 1 if identical_discs?(row, column)
+      break if !identical_discs?(row, column) || discs_connected?(total_discs)
     end
     row = indices.first
     CONNECTED_DISCS.times do
       row -= 1
-      discs_count += 1 if identical_discs?(row, column)
-      break if !identical_discs?(row, column) || discs_connected?(discs_count) || last_row?(row)
+      total_discs += 1 if identical_discs?(row, column)
+      break if !identical_discs?(row, column) || discs_connected?(total_discs)
     end
-    discs_connected?(discs_count)
+    discs_connected?(total_discs)
+  end
+
+  def leading_diagonal_connection?
+    total_discs = 1
+    row = indices.first
+    column = indices.last
+    CONNECTED_DISCS.times do
+      row += 1
+      column += 1
+      total_discs += 1 if identical_discs?(row, column)
+      break if !identical_discs?(row, column) || discs_connected?(total_discs)
+    end
+    row = indices.first
+    column = indices.last
+    CONNECTED_DISCS.times do
+      row -= 1
+      column -= 1
+      total_discs += 1 if identical_discs?(row, column)
+      break if !identical_discs?(row, column) || discs_connected?(total_discs)
+    end
+    discs_connected?(total_discs)
   end
 
   private
@@ -52,9 +73,7 @@ class ConditionsToWin
 
   attr_accessor :matrix_array, :indices, :player_id
 
-  def discs_connected?(discs_count) = (discs_count == CONNECTED_DISCS)
+  def discs_connected?(total_discs) = (total_discs == CONNECTED_DISCS)
 
-  def identical_discs?(row_index, column_index) = matrix_array[row_index][column_index] == player_id
-
-  def last_row?(row_index) = (row_index == (matrix_array.length - 1))
+  def identical_discs?(row_index, column_index) = matrix_array.dig(row_index, column_index) == player_id
 end
