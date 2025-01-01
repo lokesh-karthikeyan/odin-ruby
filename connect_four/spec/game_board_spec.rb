87b03e296535df56b64ac56c191b0fe481deea7a
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-01-01 15:54:32 +0530

    Make `Test` cases to check position
     + Added test cases for the "#valid_position?" method.
    
    All of these test cases has been successfully passed.

--- connect_four/spec/game_board_spec.rb
+++ connect_four/spec/game_board_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../lib/board/game_board'
+require_relative '../lib/game_board//game_board'
 
 # rubocop:disable Metrics/BlockLength
 describe GameBoard do
@@ -49,7 +49,7 @@ describe GameBoard do
       it 'should update from bottom-up row' do
         expect(board).to eq(model_board)
       end
-      context 'When the current selection is saved' do
+      context 'When the index is determined' do
         let(:previous_move) { game_board.instance_variable_get(:@last_move) }
 
         it 'should assign the current move to the last_move variable' do
@@ -90,7 +90,7 @@ describe GameBoard do
         expect(board).to eq(model_board)
       end
 
-      context 'When the current selection is saved' do
+      context 'When the index is determined' do
         let(:previous_move) { game_board.instance_variable_get(:@last_move) }
 
         it 'should assign the current move to the last_move variable' do
@@ -98,6 +98,60 @@ describe GameBoard do
           expect(previous_move).to eq(current_move)
         end
       end
+    end
+  end
+
+  describe '#valid_position?' do
+    context 'When position is valid in an empty column' do
+      let(:position) { 3 }
+      subject { game_board.valid_position?(position) }
+
+      it { is_expected.to be true }
+    end
+
+    context 'When position is valid in a non-empty column' do
+      let(:position) { 5 }
+      before do
+        set_board = [
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 2, 0, 1, 0, 0],
+          [0, 0, 1, 0, 1, 0, 0],
+          [2, 0, 2, 0, 2, 0, 0],
+          [1, 0, 1, 0, 1, 1, 0],
+          [2, 0, 2, 1, 2, 1, 0]
+        ]
+        game_board.instance_variable_set(:@board, set_board)
+      end
+
+      subject { game_board.valid_position?(position) }
+
+      it { is_expected.to be true }
+    end
+
+    context 'When position is invalid(out of bound)' do
+      let(:position) { 8 }
+      subject { game_board.valid_position?(position) }
+
+      it { is_expected.to be false }
+    end
+
+    context 'When position is valid, however the column is full' do
+      let(:position) { 3 }
+      before do
+        set_board = [
+          [0, 0, 1, 0, 0, 0, 0],
+          [0, 0, 2, 0, 1, 0, 0],
+          [0, 0, 1, 0, 1, 0, 0],
+          [2, 0, 2, 0, 2, 0, 0],
+          [1, 0, 1, 0, 1, 1, 0],
+          [2, 0, 2, 1, 2, 1, 0]
+        ]
+        game_board.instance_variable_set(:@board, set_board)
+      end
+
+      subject { game_board.valid_position?(position) }
+
+      it { is_expected.to be false }
     end
   end
 end
