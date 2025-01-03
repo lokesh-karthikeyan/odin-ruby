8847fdba6764c2dcd3b9e069df59dc91c16e05f7
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-01-03 17:44:27 +0530

    Refactor player	profile	creation process
     + Added "#player_name" -> It invokes the prompt name method & returns the entered input.
     + Added "#player_marker" -> Returns once the valid number selection is	made.
    
    Before Change ==> "Displayable"	module also handles "gets()".
    After Change ==> "gets()" related are taken care by new	methods from the "Player" class.

--- connect_four/lib/player/player.rb
+++ connect_four/lib/player/player.rb
@@ -24,15 +24,32 @@ class Player
 
   def create_player1_profile
     id = 1
-    name = player_name_prompt(id)
-    disc = player_disc_color_prompt(name)
+    name = player_name(id)
+    disc = player_marker(name)
     [id, name, disc]
   end
 
   def create_player2_profile
     id = 2
-    name = player_name_prompt(id)
+    name = player_name(id)
     disc = player1.disc == 'yellow' ? 'red' : 'yellow'
     [id, name, disc]
+  end
+
+  def player_name(id)
+    player_name_prompt(id)
+    gets.chomp
+  end
+
+  def player_marker(name, valid_disc_colors = %w[1 2])
+    print_available_disc_colors
+    player_disc_color_prompt(name)
+    disc = gets.chomp
+    while valid_disc_colors.none?(disc)
+      print_invalid_selection
+      player_disc_color_prompt(name)
+      disc = gets.chomp
+    end
+    disc == '1' ? 'yellow' : 'red'
   end
 end
