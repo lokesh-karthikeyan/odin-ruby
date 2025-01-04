da99ed8379e1cf7679420fcbcd2fa287ec86c2da
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-01-04 18:05:35 +0530

    Add `Game#play` & it's related methods
     + It contains a loop method which runs until "#game_over?" returns true.
     + Inside the loop, it invokes methods from same (or) different class.
     + After exiting the loop, it invokes the "#conclude_round" to end current game.
    
    Before Change ==> The components are not joined to make a game running until the end.
    After Change ==> One complete round can be run with the "#play" method.

--- connect_four/lib/game/game.rb
+++ connect_four/lib/game/game.rb
@@ -4,6 +4,18 @@ require_relative '../displayable/displayable'
 
 # Functionalities related to the game play.
 class Game
+  attr_accessor :current_player
+
+  def play
+    until game_over?
+      position = select_position_prompt
+      game_board.update_board(position, current_player.id)
+      game_board.display_board(player_disc_id)
+      switch_players
+    end
+    conclude_round
+  end
+
   def game_over?
     has_won = game_board.won?(current_player.id)
     has_tied = game_board.tie?
@@ -11,6 +23,8 @@ class Game
     result[:tie] = true if has_tied
     has_won || has_tied
   end
+
+  def switch_players = (self.current_player = current_player == player1 ? player2 : player1)
 
   def valid_choice?(choice) = choice.between?(1, 7) && game_board.valid_position?(choice)
 
@@ -27,7 +41,7 @@ class Game
 
   include Displayable
 
-  attr_accessor :player1, :player2, :game_board, :current_player, :result
+  attr_accessor :player1, :player2, :game_board, :result
 
   def initialize(player1, player2, game_board)
     self.player1 = player1
@@ -37,7 +51,20 @@ class Game
     self.result = { won: false, tie: false }
   end
 
-  def round_is_won; end
+  def select_position_prompt
+    loop do
+      print_position_prompt(current_player.name)
+      position = gets.chomp.to_i
 
-  def round_is_tied; end
+      return position if valid_choice?(position)
+
+      print_invalid_selection
+    end
+  end
+
+  def player_disc_id = { player1.id => player1.disc, player2.id => player2.disc }
+
+  def round_is_won = print_winner_announcement(current_player.name, current_player.disc)
+
+  def round_is_tied = print_game_tied_announcement(player1.disc, player2.disc)
 end
