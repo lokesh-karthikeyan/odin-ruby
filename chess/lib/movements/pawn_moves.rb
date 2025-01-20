86b01f2c07f140a0f8c9b13578d9b100a5da31ca
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-01-20 22:25:43 +0530

    Feat(Pawn Moves): Add abstract class for `Pawn Movements`

---
+++ chess/lib/movements/pawn_moves.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative 'movable'
+require_relative 'pawns/black_pawn_moves'
+require_relative 'pawns/white_pawn_moves'
+
+# Abstract class to compute Pawn's possible legal moves.
+class PawnMoves
+  include Movable
+
+  def legal_moves
+    piece_color = color(spot)
+
+    return BlackPawnMoves.new(board, spot, move_state).legal_moves if piece_color == :black
+
+    WhitePawnMoves.new(board, spot, move_state).legal_moves if piece_color == :white
+  end
+
+  private
+
+  attr_accessor :board, :spot, :move_state
+
+  def initialize(board, spot, move_state)
+    self.board = board
+    self.spot = spot
+    self.move_state = move_state
+  end
+end
