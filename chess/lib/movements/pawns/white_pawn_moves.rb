28d74914efa2fe47928116ab8ededdfd3107fd30
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-01-20 18:56:48 +0530

    Feat(Pawn Moves): Add functionality to compute `White Pawn Movements`

---
+++ chess/lib/movements/pawns/white_pawn_moves.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require_relative '../routes'
+require_relative '../movable'
+
+# Computes the White Pawn's possible legal moves.
+class WhitePawnMoves
+  include Movable
+
+  def legal_moves(moves = [])
+    first_forward_step = move_forward
+    second_forward_step = advance_two_steps?(first_forward_step) ? move_forward(double: true) : []
+    diagonal_capture_moves = capture_moves
+    moves << first_forward_step << second_forward_step << diagonal_capture_moves
+    moves.keep_if(&:any?)
+    moves
+  end
+
+  private
+
+  attr_accessor :board, :spot, :move_state
+
+  def initialize(board, spot, move_state)
+    self.board = board
+    self.spot = spot
+    self.move_state = move_state
+  end
+
+  def advance_two_steps?(first_step) = first_step.any? && spot.first.eql?(6)
+
+  def move_forward(moves = [], double: false)
+    one_step = Routes::WHITE_PAWN_SINGLE_ADVANCE
+    two_step = Routes::WHITE_PAWN_DOUBLE_ADVANCE
+
+    location = double ? compute_position(two_step, spot) : compute_position(one_step, spot)
+    return moves unless range?(location) && null_piece?(location)
+
+    moves << location if null_piece?(location)
+    moves.length.eql?(1) ? moves.flatten : moves
+  end
+
+  def capture_moves(moves = [])
+    left_diagonal_location = compute_position(Routes::WHITE_PAWN_LEFT_CAPTURE, spot)
+    right_diagonal_location = compute_position(Routes::WHITE_PAWN_RIGHT_CAPTURE, spot)
+
+    moves << left_diagonal_location if capture_move?(left_diagonal_location) || left_side_en_passant?
+    moves << right_diagonal_location if capture_move?(right_diagonal_location) || right_side_en_passant?
+    moves.length.eql?(1) ? moves.flatten : moves
+  end
+
+  def capture_move?(location) = range?(location) && enemy?(location)
+
+  def left_side_en_passant?
+    location = compute_position(Routes::LEFT_SIDE_EN_PASSANT, spot)
+    range?(location) && en_passant?(location)
+  end
+
+  def right_side_en_passant?
+    location = compute_position(Routes::RIGHT_SIDE_EN_PASSANT, spot)
+    range?(location) && en_passant?(location)
+  end
+
+  def en_passant?(location) = move_state.en_passant_target == fetch_spot_label(location) && enemy_pawn?(location)
+
+  def fetch_spot_label(location) = board.dig(location.first, location.last).label.downcase
+
+  def enemy_pawn?(location) = enemy?(location) && board.dig(location.first, location.last).piece.type == :Pawn
+end
