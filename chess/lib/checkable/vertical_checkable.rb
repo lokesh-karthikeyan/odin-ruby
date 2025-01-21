7d36a002f8018510dbd0bc020caeae98df1b0d0a
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-01-21 11:48:39 +0530

    Feat(Check): Add functionality to compute `Vertical Check`

---
+++ chess/lib/checkable/vertical_checkable.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative '../movements/routes'
+require_relative '../movements/movable'
+require_relative './inspect_enemy_piece'
+
+# Inspect King's safety if it's in a position of Check (or) Mate.
+module Checkable
+  # Inspect King's safety in a vertical path
+  class VerticalCheckable
+    include Movable
+    include InspectEnemyPiece
+
+    def enemy_whereabouts(locations = [])
+      ROUTES.each { |route| locations.append(*explore_path(route)) }
+      locations.keep_if { |location| rook?(location) || queen?(location) || king_nearby?(location) }
+      locations
+    end
+
+    private
+
+    ROUTES = Routes::VERTICAL
+
+    attr_accessor :board, :spot
+
+    def initialize(board, spot)
+      self.board = board
+      self.spot = spot
+    end
+  end
+end
