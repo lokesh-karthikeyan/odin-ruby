1905bbc5458461892f242094dcf70937e528d0b1
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-01-22 19:32:21 +0530

    Feat(Rook Moves): Add implementation of `Rook Movements`

---
+++ chess/lib/movements/rook_moves.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'routes'
+require_relative 'movable'
+
+# Computes the Rook's possible legal moves.
+class RookMoves
+  include Movable
+
+  def legal_moves(moves = [])
+    ROUTES.each { |route| moves.append(*travel(route, spot)) }
+    moves
+  end
+
+  private
+
+  ROUTES = Routes::HORIZONTAL + Routes::VERTICAL
+
+  attr_accessor :board, :spot, :move_state
+
+  def initialize(board, spot, move_state)
+    self.board = board
+    self.spot = spot
+    self.move_state = move_state
+  end
+end
