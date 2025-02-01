cc43facd2ad972b7e14c4caa6d43e9002a826ee4
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-02-02 00:45:25 +0530

    Perf: Make `.flush_data` as "Static" method
     + Before Change --> In order to delete data, need to instantiate with game data.
     + After Change --> It just finds the file & deletes the data, as it's a "Static method".

--- chess/lib/data_transferable/serialize.rb
+++ chess/lib/data_transferable/serialize.rb
@@ -4,17 +4,20 @@
 module DataTransferable
   # Serializes the given data in a storable format.
   class Serialize
+    class << self
+      def flush_data = File.truncate('data/game_data.yml', 0)
+    end
+
     def store_data
       Dir.mkdir('data') unless Dir.exist?('data')
-      self.file_name = 'data/game_data.yml'
+      file_name = 'data/game_data.yml'
+
       File.write(file_name, game_data.to_yaml)
     end
 
-    def flush_data = File.truncate(file_name, 0)
-
     private
 
-    attr_accessor :game_data, :file_name
+    attr_accessor :game_data
 
     def initialize(game_data)
       self.game_data = game_data
