9f73e6a82ea822dcfe9fe46b5948cc1e157d1d9d
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-02-02 01:33:31 +0530

    Refactor: Change arguments from `Array` to `String`

--- chess/lib/inspect/half_move_clock_inspector.rb
+++ chess/lib/inspect/half_move_clock_inspector.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
+require_relative '../movements/movable'
+
 # Inspects if the "Half-Move clock" can be incremented.
 class HalfMoveClockInspector
-  def increment_count?(source, target) = source_piece_validator(source) && target_piece_validator(target)
+  include Movable
+
+  def increment_count?(source_piece, target_piece)
+    source_piece_validator(source_piece) && target_piece_validator(target_piece)
+  end
 
   private
 
-  def source_piece_validator(source)
-    piece, = source
-    piece != :Pawn
-  end
+  def source_piece_validator(piece) = piece != :Pawn
 
-  def target_piece_validator(target) = target.all? { |attributes| attributes.to_s.strip.empty? }
+  def target_piece_validator(piece) = piece.to_s.strip.empty?
 end
